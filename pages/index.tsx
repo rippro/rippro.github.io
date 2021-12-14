7326ed071b60e1494159069aff90dde231cf3a2c
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-15 01:23:12 +0900

    feat: indexのarticleを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,16 @@
 import type { NextPage } from "next";
 import { Layout } from "../components/Layout";
 import Twitter from "../components/Twitter";
+import IndexArticles from "../components/IndexArticles";
 
 const Home: NextPage = () => {
   return (
     <Layout
-      home
       title="RiPPro - 立命館大学情報理工学部プロジェクト団体"
       description="立命館大学情報理工学部プロジェクト団体 RiPPro (Ritsumeikan Programming Project) は、ACM-ICPC (プログラミングの学生世界大会) などの大会で入賞することを目的として結成されました。"
     >
       <div>
-        <h1>ようこそ RiPPro へ</h1>
+        <IndexArticles />
       </div>
       <Twitter />
     </Layout>
