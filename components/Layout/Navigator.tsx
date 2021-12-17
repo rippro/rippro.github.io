91960e0b273cd82b2f4ed602358230dcdc78b072
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-17 18:48:09 +0900

    feat: update naviber

--- components/Layout/Navigator.tsx
+++ components/Layout/Navigator.tsx
@@ -3,32 +3,45 @@
 import { VFC } from 'react'
 import Link from 'next/link'
 
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
+
 import styles from './Navigator.module.css'
 
 const Navigator: VFC = () => {
   return (
-    <div className={styles.navi}>
-      <div className={styles.navibutton}>
-        <Link href="/">Top</Link>
-      </div>
-      <div className={styles.navibutton}>
-        <b>
-          <Link href="/welcome">新歓情報</Link>
-        </b>
-      </div>
-      <div className={styles.navibutton}>
-        <Link href="/event">Event</Link>
-      </div>
-      <div className={styles.navibutton}>
-        <Link href="/contact">Contact</Link>
-      </div>
-      <div className={styles.navibutton}>
-        <Link href="/links">Links</Link>
-      </div>
-      <div className={styles.navibutton}>
-        <Link href="/menbers">部員向け</Link>
-      </div>
-    </div>
+    <Stack spacing={1} direction="row">
+      <Link href="/">
+        <Button variant="text" className={styles.navibutton}>
+          Top
+        </Button>
+      </Link>
+      <Link href="/welcome">
+        <Button variant="text" className={`${styles.welcome} ${styles.navibutton}`}>
+          新歓情報
+        </Button>
+      </Link>
+      <Link href="/event">
+        <Button variant="text" className={styles.navibutton}>
+          Event
+        </Button>
+      </Link>
+      <Link href="/contact">
+        <Button variant="text" className={styles.navibutton}>
+          Contact
+        </Button>
+      </Link>
+      <Link href="/links">
+        <Button variant="text" className={styles.navibutton}>
+          Links
+        </Button>
+      </Link>
+      <Link href="/menbers">
+        <Button variant="text" className={styles.navibutton}>
+          部員向け
+        </Button>
+      </Link>
+    </Stack>
   )
 }
 
