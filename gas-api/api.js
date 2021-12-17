1e8fb4224462dca6a4ff5ebe6a99c47dde2076b5
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-17 19:29:52 +0900

    refactor: getUserIDsをきれいにした

--- gas-api/api.js
+++ gas-api/api.js
@@ -1,31 +1,26 @@
 /* ユーザ情報取得API */
-const getUsersInfo = () => {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('index')
-  const lastRow = sheet.getLastRow()
-  const data = sheet.getRange(`A2:A${sheet.getLastRow()}`).getValues()
-  let responseData = []
-  for (let r = 0; r < lastRow - 1; r++) {
-    responseData.push(data[r][0])
-  }
-  return responseData
-}
-
 function doGet(e) {
   const usersInfo = getUsersInfo()
   const json = JSON.stringify(usersInfo)
   return ContentService.createTextOutput().setMimeType(ContentService.MimeType.JSON).setContent(json)
 }
 
-/* レート更新API */
+/* ユーザID取得API */
+const getUserIDs = () => {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('index')
+  const data = sheet.getRange(`A2:A${sheet.getLastRow()}`).getValues()
+  let responseData = data.map((user) => user[0])
+  return responseData
+}
 
+/* レート更新API */
 const saveUserIDs = () => {
   // シートをクリア
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('usersInfo')
   sheet.clear()
 
   // ユーザIDを取得
-  const userIDs = getUsersInfo()
-
+  const userIDs = getUserIDs()
   const userInfo = userIDs.map((userID) => {
     const ACCount = request(userID)
     return [userID, ACCount]
