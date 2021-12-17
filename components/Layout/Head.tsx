c933a3d0d6b747008232c48a7c24b98d4f471333
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-17 18:00:50 +0900

    feat: ナビゲーションバーを上に移動した

--- components/Layout/Head.tsx
+++ components/Layout/Head.tsx
@@ -2,23 +2,18 @@
 
 import { VFC } from 'react'
 import Link from 'next/link'
-
+import Navigator from './Navigator'
 import styles from './Head.module.css'
 
 const Header: VFC = () => {
   return (
     <div className={styles.head}>
-      <div className={styles.tagline}>
-        立命館大学情報理工学部プロジェクト団体
-        <br /> Programming Contest Project Team, Ritsumeikan University Faculty of Information Science and Engineering
-      </div>
-      <h1>
-        <Link href="/">
-          <a>
-            <img src="/rippro-rogo.png" alt="rippro-rogo" width={202} height={62} />
-          </a>
-        </Link>
-      </h1>
+      <Link href="/">
+        <a className={styles.logo}>
+          <img src="/rippro-rogo.png" alt="rippro-rogo" />
+        </a>
+      </Link>
+      <Navigator />
     </div>
   )
 }
