185f55f2a6892d279d6c4ae8547ff002717f8933
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-19 19:21:15 +0900

    fix: gasのuriを変更

--- components/atcoder/AtCoderGraph.tsx
+++ components/atcoder/AtCoderGraph.tsx
@@ -30,7 +30,7 @@ class AtCoderGraph extends React.Component<{}, State> {
   }
   getUsersInfo = () => {
     const url =
-      'https://script.google.com/macros/s/AKfycbyrbKATwoiI2W9CZOydrj46Z9DQY3NYysXoWx_8_DYObC6_c5JRcfPSgmpbZzzgNSWL/exec'
+      'https://script.google.com/macros/s/AKfycbxCUIAch1pPH3mvu0czg01F6w2S_4FjvhDDBBu0oWySDoMjbjy9YdNtnj0SXGb0sMDE/exec'
     this.request(url).then((responseData: any) => {
       const usersInfo: UserInfoType[] = responseData
         .map((data: any) => {
